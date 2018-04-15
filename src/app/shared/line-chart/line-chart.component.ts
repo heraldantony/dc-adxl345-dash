ddf7120901d0299dfd1f98d4e94137e50ae8cf94
heraldantony <herald.antony@zoho.com>
2018-04-15 23:48:13 +0530

    initial version with dummy data

---
+++ src/app/shared/line-chart/line-chart.component.ts
@@ -0,0 +1,106 @@
+import { Component, OnInit, OnChanges, ViewChild, ElementRef, Input, ViewEncapsulation } from '@angular/core';
+import * as d3 from 'd3';
+
+@Component({
+  selector: 'app-line-chart',
+  templateUrl: './line-chart.component.html',
+  styleUrls: ['./line-chart.component.scss'],
+encapsulation: ViewEncapsulation.None
+})
+export class LineChartComponent implements OnInit {
+
+ @ViewChild('linechart') private chartContainer: ElementRef;
+  @Input() private data: Array<any>;
+  private margin: any = { top: 20, bottom: 20, left: 20, right: 20};
+  private chart: any;
+  private line: any;
+  private width: number;
+  private height: number;
+  private xScale: any;
+  private yScale: any;
+  private colors: any;
+  private xAxis: any;
+  private yAxis: any;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  ngAfterViewInit() {
+    setTimeout(()=>{
+    this.createChart();
+    if (this.data) {
+      this.updateChart();
+    }
+    }, 1);
+  }
+
+  ngOnChanges() {
+    if (this.chart) {
+      this.updateChart();
+    }
+  }
+
+  createChart() {
+    const element = this.chartContainer.nativeElement;
+    this.width = element.offsetWidth - this.margin.left - this.margin.right;
+    this.height = element.offsetHeight - this.margin.top - this.margin.bottom;
+    const svg = d3.select(element).append('svg')
+      .attr('width', element.offsetWidth)
+      .attr('height', element.offsetHeight);
+
+    // chart plot area
+    this.chart = svg.append('g')
+        .attr('transform', `translate(${this.margin.left}, ${this.margin.top})`);
+
+    // define X & Y domains
+    const xDomain = this.data.map(d => d[0]);
+    const yDomain = [0, d3.max(this.data, d => d[1])];
+
+    // create scales
+    this.xScale = d3.scaleBand().padding(0.1).domain(xDomain).rangeRound([0, this.width]);
+    this.yScale = d3.scaleLinear().domain(yDomain).range([this.height, 0]);
+
+    // bar colors
+    this.colors = d3.scaleLinear().domain([0, this.data.length]).range(<any[]>['red', 'blue']);
+
+    // x & y axis
+    this.xAxis = svg.append('g')
+      .attr('class', 'axis axis-x')
+      .attr('transform', `translate(${this.margin.left}, ${this.margin.top + this.height})`)
+      .call(d3.axisBottom(this.xScale));
+    this.yAxis = svg.append('g')
+      .attr('class', 'axis axis-y')
+      .attr('transform', `translate(${this.margin.left}, ${this.margin.top})`)
+      .call(d3.axisLeft(this.yScale));
+      this.line = d3.line()
+    .x(d => this.xScale(d[0]))
+    .y(d => this.yScale(d[1]));
+
+    this.chart.append("path")
+      .attr("fill", "none")
+      .attr("class", "line")
+      .attr("stroke", "steelblue")
+      .attr("stroke-linejoin", "round")
+      .attr("stroke-linecap", "round")
+      .attr("stroke-width", 1.5)
+      .attr("d", this.line(this.data));
+
+  }
+
+  updateChart() {
+    // update scales & axis
+    this.xScale.domain(this.data.map(d => d[0]));
+    this.yScale.domain([0, d3.max(this.data, d => d[1])]);
+    this.colors.domain([0, this.data.length]);
+    this.xAxis.transition().call(d3.axisBottom(this.xScale));
+    this.yAxis.transition().call(d3.axisLeft(this.yScale));
+
+    
+    
+       this.chart.select(".line")   // change the line
+            .transition(750)
+            .attr("d", this.line(this.data));
+}
+
+}
